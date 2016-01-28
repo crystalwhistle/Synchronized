88fda38e4bb7fb38d7013ae20a54f786a4430f0a
Crystal <crystal1770@Gmail.com>
2016-01-28 23:10:14 +0000

    menu, settings, hover

--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,20 @@ exports.view = function(req, res){
       { 'name': "Crystal's 21st Birthday",
         'date': "March 24 2016",
         'image': '21party.jpeg',
-        'id': 'project1'
+        'id': 'project1',
+        'setIMG': 'settings.png'
       },
       { 'name': 'Launch Party',
         'date': "May 15 2016",
         'image': 'launchparty.jpg',
-        'id': 'project2'
+        'id': 'project2',
+        'setIMG': 'settings.png'
       },
       { 'name': 'EDC Pregame',
         'date': "June 21 2016",
         'image': 'edc.jpg',
-        'id': 'project3'
+        'id': 'project3',
+        'setIMG': 'settings.png'
       },
       
     ]
