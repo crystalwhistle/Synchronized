c4b7e5a4da0e75e530884100e6949fe1028e165f
Crystal <crystal1770@Gmail.com>
2016-02-12 10:06:44 +0000

    Fix create new event

--- routes/createNewEvent.js
+++ routes/createNewEvent.js
@@ -4,11 +4,16 @@ exports.addEvent = function(req, res){
     console.log(data);
     res.render('createNewEvent', data);
 
-    var newEvent = {"name" : req.query.name,
-					"date" : req.query.date,
-					"image": req.query.image,
-					"id": "",
-					"setIMG": "settings.png"
-					}
+    if (req.query.name && req.query.date) {
+    	var newEvent = {"name" : req.query.name,
+						"date" : req.query.date,
+						"image": req.query.image,
+						"id": "",
+						"setIMG": "settings.png"
+						}
 	data["projects"].push(newEvent);
+	}
+	else {
+
+	}
 }
