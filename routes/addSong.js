cf6a00e7136865d1c8e7d3a77b2e6e906e541fcd
dchour9 <davidchour9@gmail.com>
2016-02-12 02:31:43 +0000

    adding songs works

--- routes/addSong.js
+++ routes/addSong.js
@@ -1,3 +1,20 @@
+var playlist1 = require('../playlist1.json');
+
 exports.view = function(req, res){
-    res.render('addSong');
-}
+  console.log("infunction");
+  var addedNotification;
+  var songName = req.query.songTitle;
+  var artist = req.query.artist;
+  if(songName && artist){
+  	var newSong = {
+    	"songName": songName,
+    	"artist": artist,
+    	"embedURL": "http://lorempixel.com/400/400/people"
+  	}
+  	playlist1["playlist1"].push(newSong);
+  	addedNotification = songName + " by " + artist + " was added!";
+  }
+  res.render('addSong', {
+  	'notification': addedNotification
+  });
+};
