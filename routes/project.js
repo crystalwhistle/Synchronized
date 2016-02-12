67948801b75a22b68810e3ba9a3ba25ba2c4d7d5
Crystal <crystal1770@Gmail.com>
2016-02-12 00:13:15 +0000

    David's Soundcloud add

--- routes/project.js
+++ routes/project.js
@@ -1,7 +1,23 @@
+var playlist1 = require('../playlist1.json');
+exports.viewProject = function(req,res){
+	//var name = req.params.name;
+	console.log(playlist1);
+  var songName = req.query.songTitle;
+  var artist = req.query.artist;
+  var newSong = {
+    "songName": songName,
+    "artist": artist,
+    "embedURL": "http://lorempixel.com/400/400/people"
+  }
+  playlist1["playlist1"].push(newSong);
+  res.render('project', playlist1);
+};
+
+/*
 exports.viewProject = function(req,res){
 	var name = req.params.name;
 	console.log("The project name is " + name)
 	res.render('project', {
 		'projectName': name
 	});	
-};
+};*/
