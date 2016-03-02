b381b360257d5eeff0d595913abfe2f1f9e63d74
Crystal <crystal1770@Gmail.com>
2016-03-02 04:00:48 +0000

    Experiment testing

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,16 +1,23 @@
-'use strict';
-
-// Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
-})
-
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	// add any functionality and listeners you want here
-	$('#newEvent').click(newEventButtonClicked);
+	var loadDate = new Date();
+
+	$('#newEvent').on('click', function (event) {
+		var now = new Date();
+		var elapsed = now - loadDate;
+
+		console.log("Elapsed:" + elapsed);
+		console.log("Create new event button clicked");
+
+		//ga("send", "timing", "newEvent", "click", elapsed);
+		ga("send", "event", "newEvent", "click", elapsed);
+		//ga("send", "event", "newEventButton", "click");
+
+	})
+	//$('#newEvent').click(newEventButtonClicked);
 }'use strict';
 
 // Call this function when the page loads (the "ready" event)
@@ -18,7 +25,8 @@ $(document).ready(function() {
 	initializePage();
 })
 
+/*
 function newEventButtonClicked(e){
 	console.log("Create new event button clicked")
 	ga("send", "event", "newEvent", "click");
-}
+}*/
