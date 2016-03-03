e6acb40182297d406a3f2b082009f41220dee2c1
Crystal <crystal1770@Gmail.com>
2016-03-03 20:16:44 +0000

    GA A/B testing

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,28 +5,30 @@ function initializePage() {
 	// add any functionality and listeners you want here
 	var loadDate = new Date();
 
-	$('#newEvent').on('click', function (event) {
+	$('#newEventVA').on('click', function (event) {
 		var now = new Date();
 		var elapsed = now - loadDate;
 
 		console.log("Elapsed:" + elapsed);
 		console.log("Create new event button clicked");
 
-		//ga("send", "timing", "newEvent", "click", elapsed);
-		ga("send", "event", "newEvent", "click", elapsed);
-		//ga("send", "event", "newEventButton", "click");
-
+		ga("send", "event", "newEventVA", "click", elapsed);
 	})
-	//$('#newEvent').click(newEventButtonClicked);
+
+
+	$('#newEventVB').on('click', function (event) {
+		var now = new Date();
+		var elapsed = now - loadDate;
+
+		console.log("Elapsed:" + elapsed);
+		console.log("Create new event button clicked");
+
+		ga("send", "event", "newEventVB", "click", elapsed);
+	})
+
 }'use strict';
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
 })
-
-/*
-function newEventButtonClicked(e){
-	console.log("Create new event button clicked")
-	ga("send", "event", "newEvent", "click");
-}*/
